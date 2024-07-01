76afd6a84bce269c58695502391a9e1d80ef0be6
dmendie <116080582+dmendie@users.noreply.github.com>
2024-07-01 15:43:15 +0000

    update on weather api

--- src/HNG.Api.Client/Extensions/IpInfoService.cs
+++ src/HNG.Api.Client/Extensions/IpInfoService.cs
@@ -1,7 +1,10 @@
 ﻿using HNG.Abstractions.Helpers;
+using HNG.Abstractions.Models;
 using IPinfo;
 using IPinfo.Models;
 using Microsoft.Extensions.Caching.Memory;
+using OpenWeatherMap;
+using OpenWeatherMap.Models;
 
 namespace HNG.Api.Client.Extensions
 {
@@ -10,16 +13,27 @@ namespace HNG.Api.Client.Extensions
     /// </summary>
     public class IpInfoService
     {
+        private readonly AppSettings _appSettings;
         private readonly IPinfoClient _ipInfoClient;
+        private readonly IOpenWeatherMapService _openWeatherMapService;
         private readonly IMemoryCache _cache;
         private readonly TimeSpan _cacheDuration = TimeSpan.FromDays(30);
+        private readonly TimeSpan _cacheWeatherDuration = TimeSpan.FromHours(1);
         private const int PrefixLength = 24;
 
         /// <summary>
         /// constructor
         /// </summary>
-        public IpInfoService(IPinfoClient ipInfoClient, IMemoryCache cache)
+        public IpInfoService(IPinfoClient ipInfoClient, IMemoryCache cache, AppSettings appSettings)
         {
+            _appSettings = appSettings;
+            _openWeatherMapService = new OpenWeatherMapService(new OpenWeatherMapOptions
+            {
+                ApiKey = _appSettings.OpenWeatherMap.ApiKey,
+                ApiEndpoint = _appSettings.OpenWeatherMap.ApiEndpoint,
+                Language = _appSettings.OpenWeatherMap.Language,
+                UnitSystem = _appSettings.OpenWeatherMap.UnitSystem
+            });
             _ipInfoClient = ipInfoClient;
             _cache = cache;
         }
@@ -45,6 +59,27 @@ namespace HNG.Api.Client.Extensions
             var response = await _ipInfoClient.IPApi.GetDetailsAsync(ip);
             _cache.Set(ipRange, response, _cacheDuration);
             return response;
+        }
+
+        /// <summary>
+        /// Get weather information for Ip
+        /// </summary>
+        public async Task<WeatherInfo?> GetWeatherInfoAsync(string Ip, string? Lat, string? Lon)
+        {
+            var ipRange = IpRangeHelper.GetIpRange(Ip, PrefixLength);
+            if (_cache.TryGetValue(ipRange, out WeatherInfo? cachedResponse))
+            {
+                return cachedResponse;
+            }
+
+            if (string.IsNullOrWhiteSpace(Lat) || string.IsNullOrWhiteSpace(Lon))
+            {
+                return null;
+            }
+
+            var weatherInfo = await _openWeatherMapService.GetCurrentWeatherAsync(Convert.ToDouble(Lat), Convert.ToDouble(Lon));
+            _cache.Set(ipRange, weatherInfo, _cacheWeatherDuration);
+            return weatherInfo;
         }
     }
 }
